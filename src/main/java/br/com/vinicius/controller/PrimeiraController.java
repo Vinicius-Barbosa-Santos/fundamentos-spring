4409d3aea1a97fdb4e4b4c3994e13e1cb4d9a088
Vinicius-Barbosa-Santos <viniciusfoxsantos@gmail.com>
2024-05-11 14:04:00 -0300

    chore: body params

--- src/main/java/br/com/vinicius/controller/PrimeiraController.java
+++ src/main/java/br/com/vinicius/controller/PrimeiraController.java
@@ -4,6 +4,8 @@ import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -28,4 +30,11 @@ public class PrimeiraController {
 	public String metodoComQueryParams2(@RequestParam Map<String, String> allParams) {
         return "o parâmetro com metodoQueryParams é " + allParams.entrySet();
 	} 
+
+    @PostMapping("/metodoComBodyParams")
+    public String metodoComBodyParams(@RequestBody Usuario usuario) {
+        return "metodoComBodyParams " + usuario.username;
+    }
+
+    record Usuario(String username) {}
 }
